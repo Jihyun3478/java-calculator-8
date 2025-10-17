011079c60ef2ae81ab7180b4f95f387520ab3ee0
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-17 17:37:48 +0900

    refactor: Calculator 클래스 domain 패키지로 이동

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import calculator.domain.Calculator;
+
 public class CalculatorTest {
     private Calculator calculator;
 
