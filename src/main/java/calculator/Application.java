606d1d7a650184ad063c4ee3d0b4a3c4941bf283
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-17 17:18:33 +0900

    feat: 계산 결과를 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,5 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(input);
+
+        System.out.println("결과 : " + result);
     }
 }
