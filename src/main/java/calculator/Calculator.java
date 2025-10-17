8f097e8501906ed0582dffcf4a5ca7142eb654d8
Jihyun3478 <leejihyun3478@gmail.com>
2025-10-17 17:17:05 +0900

    feat: 사용자로부터 문자열을 입력받는 기능 구현
    사용자로부터 문자열을 입력받는 기능 추가
    - 입력된 값이 없는 경우, 예외 발생
    - 숫자가 양수가 아닌 경우, 예외 발생
    - 잘못된 형식인 경우, 예외 발생

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,12 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
     public int calculate(String input) {
+        validateInput(input);
         String[] splits = extractNumbers(input);
 
         int sum = 0;
@@ -16,7 +18,39 @@ public class Calculator {
         return sum;
     }
 
+    private void validateInput(String input) {
+        if (Objects.isNull(input) || input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 비어있습니다.");
+        }
+
+        if (input.contains("0") || input.contains("-1")) {
+            throw new IllegalArgumentException("입력되는 숫자는 양수이어야 합니다.");
+        }
+
+        if (!(input.contains("//") && (input.contains("\n") || input.contains("\\n")))) {
+            validateDefaultFormat(input);
+        }
+    }
+
+    private void validateDefaultFormat(String input) {
+        if (input.matches("[,:]+")) {
+            throw new IllegalArgumentException("구분자만 입력되었습니다.");
+        }
+
+        if (input.matches(".*[^0-9,:].*")) {
+            throw new IllegalArgumentException("잘못된 구분자입니다.");
+        }
+
+        if (input.matches(".*[,:]{2,}.*")) {
+            throw new IllegalArgumentException("잘못된 형식입니다.");
+        }
+    }
+
     private String[] extractNumbers(String input) {
+        if (input.contains("\\n")) {
+            input = input.replace("\\n", "\n");
+        }
+
         if (input.contains("//") && input.contains("\n")) {
             return extractWithCustomDelimiter(input);
         }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,17 +1,27 @@
 package calculator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
 
     @Test
     @DisplayName("기본 구분자로 숫자를 추출해 합을 계산한다.")
     public void 기본_구분자로_추출해_계산() {
         String input = "1,2:3";
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
         int result = calculator.calculate(input);
 
         assertEquals(6, result);
@@ -21,9 +31,36 @@ public class CalculatorTest {
     @DisplayName("커스텀 구분자로 숫자를 추출해 합을 계산한다.")
     public void 커스텀_구분자로_추출해_계산() {
         String input = "//;\n4;5;6";
-        Calculator calculator = new Calculator();
+        calculator = new Calculator();
         int result = calculator.calculate(input);
 
         assertEquals(15, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("입력된 값이 없는 경우, 예외가 발생한다.")
+    public void 입력된_값이_없는_경우_예외_발생(String input) {
+        calculator = new Calculator();
+        assertAll(
+            () -> assertThatThrownBy(() -> calculator.calculate(input)).isInstanceOf(IllegalArgumentException.class),
+            () -> assertThatThrownBy(() -> calculator.calculate(null)).isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("숫자가 양수가 아닌 경우, 예외가 발생한다.")
+    public void 숫자가_양수가_아닌_경우_예외_발생() {
+        String input = ",;";
+        assertThatThrownBy(() -> calculator.calculate(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {",;", "1*2?3", "1,,2;3"})
+    @DisplayName("잘못된 형식인 경우, 예외가 발생한다.")
+    public void 잘못된_형식인_경우_예외_발생() {
+        String input = ",;";
+        assertThatThrownBy(() -> calculator.calculate(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
